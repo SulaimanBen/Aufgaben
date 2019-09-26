7690b6a0546922e0e42eaea0523847e77feb54f4
CC-Student <CC-Student@MININT-BPDVKD7.fritz.box>
2019-09-26 07:42:56 +0200

    update

--- Aufgaben/src/AufgabenKlassenPerson/KlassenPerson.java
+++ Aufgaben/src/AufgabenKlassenPerson/KlassenPerson.java
@@ -19,6 +19,27 @@ public class KlassenPerson {
 		System.out.println(p1.equals(p2)); // false
 		Person p3=new Person("Ben","sulaiman",1992,80689);
 		System.out.println(p2.equals(p3));
+		
+		// Statische Methode ´getMustermann´ testen
+		
+		System.out.println(getMustermann());
+		
+	}
+	
+	/*
+	 *  Erzeugen Sie bitte eine statische Methode getMustermann die eine neue Person 'Max Mustermann,
+	 *  Geburtsjahr: 1960, Plz: 12345' bildet und zurück liefert. 
+	 *  Testen Sie die Methode.
+	 */
+	
+	static String getMustermann() {
+		Person p=new Person(p.setVorname("Max"), p.setNachname("Mustermann"),p.setGeburtsjahr(1960),p.setPostleitzahl(12345));
+		p.setVorname("Max");
+		p.setNachname("Mustermann");
+		p.setGeburtsjahr(1960);
+		p.setPostleitzahl(12345);
+		return p.getVorname()+" "+ p.getNachname()+" , Geburtsjahr : "+p.getGeburtsjahr()
+		+" , Plz : "+p.getPostleitzahl();
 	}
 
 }
