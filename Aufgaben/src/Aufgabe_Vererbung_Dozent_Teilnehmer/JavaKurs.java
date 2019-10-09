5f729672bf6785ad8078b475a057b5c23a77bfe8
CC-Student <CC-Student@MININT-BPDVKD7.fritz.box>
2019-10-09 06:32:26 +0200

    update

--- Aufgaben/src/Aufgabe_Vererbung_Dozent_Teilnehmer/JavaKurs.java
+++ Aufgaben/src/Aufgabe_Vererbung_Dozent_Teilnehmer/JavaKurs.java
@@ -1,7 +1,11 @@
 package Aufgabe_Vererbung_Dozent_Teilnehmer;
 
-public class JavaKurs {
+public class JavaKurs extends Dozent{
 	
 	int Nummer;
+	public JavaKurs(String name, String iD) {
+		super(name, iD);
+		// TODO Auto-generated constructor stub
+	}
 
 }
