29890a56a2271388347446f4f013c0e977477aa5
ASUL167 <ASUL167@PC10146555.cc-college.gecs.loc>
2019-10-23 12:49:53 +0200

    update

---
+++ Aufgaben/src/aufgabe_richtige_ubeladene_finden/RichtigeUberladeneFinden.java
@@ -0,0 +1,23 @@
+package aufgabe_richtige_ubeladene_finden;
+
+public class RichtigeUberladeneFinden {
+	
+		
+//		static void m(float x) { System.out.println("float"); }
+		
+//		static void m(Integer x) { System.out.println("Integer"); }
+//		static void m(long x) { System.out.println("long"); }
+//		static void m(Object x) { System.out.println("Object"); }
+//		static void m(Number x) { System.out.println("Number"); }
+		static void m(int... x) { System.out.println("int..."); }
+		
+		
+		
+		public static void main(String... args) {
+			Integer var = 1;
+
+			
+			m( var ); 
+			
+	}
+}
