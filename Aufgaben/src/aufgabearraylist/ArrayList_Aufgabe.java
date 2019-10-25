06d75284252270c0bfa1754f3c0eaf8e855c088f
CC-Student <CC-Student@MININT-BPDVKD7.fritz.box>
2019-10-25 05:51:44 +0200

    update

---
+++ Aufgaben/src/aufgabearraylist/ArrayList_Aufgabe.java
@@ -0,0 +1,91 @@
+package aufgabearraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+
+public class ArrayList_Aufgabe {
+	
+	static List<Integer> createZufaelligArrayList(int size , int min , int max){
+		Random random = new Random();
+		List<Integer> list = new ArrayList<>();
+		
+		for (int i = 0; i < size; i++) {
+			list.add(random.nextInt( max - min + 1 ) + min );
+		}
+		
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		a1();
+		a2();
+		a3();
+		a4();
+		a5();
+		
+	}
+
+	private static void a5() {
+		System.out.println("********** A5 **********");
+		
+		
+		
+	}
+
+	private static void a4() {
+		System.out.println("********** A4 **********");
+		List<Integer> list = createZufaelligArrayList(10, 0, 50);
+		Object[] arr = list.toArray();
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+	private static void a3() {
+		System.out.println("********** A3 **********");
+		
+		List<Integer> list = createZufaelligArrayList(10, 0, 50);
+		System.out.println("list Befor replaceAll() : "+list);
+		list.replaceAll(num -> 0);
+		System.out.println("list After replaceAll() : "+list);
+	}
+
+	private static void a2() {
+		System.out.println("********** A2 **********");
+		
+		List<Integer> list = createZufaelligArrayList(10, 0, 50);
+		System.out.println("list Befor removeIf() : "+list);
+		
+		list.removeIf(num ->(num % 2 != 0));
+		System.out.println("list After removeIf() : "+list);
+		
+	}
+
+	private static void a1() {
+		System.out.println("********** A1 **********");
+		
+		List<String> wochentag = new ArrayList<>();
+		
+		wochentag.add("mo");
+		wochentag.add("di");
+		wochentag.add("mi");
+		wochentag.add("do");
+		wochentag.add("fr");
+		System.out.println(wochentag);
+		
+		Iterable<String> iterable = wochentag;
+		Iterator<String> iterator = iterable.iterator();
+		
+		while( iterator.hasNext() ) {
+			String e = iterator.next();
+			System.out.println(e);
+		}
+	}
+
+}
