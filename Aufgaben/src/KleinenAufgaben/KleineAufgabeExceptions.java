982f41b0e37410d080b9cb2d060035e03c420596
ASUL167 <ASUL167@PC10146555.cc-college.gecs.loc>
2019-11-05 12:56:44 +0100

    update

---
+++ Aufgaben/src/KleinenAufgaben/KleineAufgabeExceptions.java
@@ -0,0 +1,27 @@
+package KleinenAufgaben;
+
+public class KleineAufgabeExceptions {
+
+	public static void main(String[] args) {
+		
+		String name = "Jerry";
+		name = formatName(name);
+		System.out.println("Name: " + name); // JERRY
+		
+		try {
+			name = formatName(null);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		formatName("Do");
+		
+	}
+
+	private static String formatName(String name) {
+		if(name.length() < 3) {
+			throw new IllegalArgumentException("Der Name muss mindestens 3 Zeichen habem");
+		}
+		return name.toUpperCase();
+	}
+}
