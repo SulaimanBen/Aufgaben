dbaaec736669709ece1a5ef93a6b36d1a678ab37
CC-Student <CC-Student@MININT-BPDVKD7.fritz.box>
2019-11-11 19:09:27 +0100

    update

---
+++ Aufgaben/src/aufgabe_date_time_calendar/DateTimeCalendar.java
@@ -0,0 +1,32 @@
+package aufgabe_date_time_calendar;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeCalendar {
+
+	public static void main(String[] args) {
+		
+		printCalendar(4, 2010); // April 2010
+		
+	}
+
+	private static void printCalendar(int month, int year) {
+		System.out.println("-------------------------");
+		LocalDate date = LocalDate.of(year, month, 1);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMMM y");
+		String dateFormat = date.format(fmt);
+		System.out.println(dateFormat);
+		
+		System.out.println("-------------------------");
+		fmt = DateTimeFormatter.ofPattern("EEEE");
+		
+		String textFormat = "| %02d |  %-10s  |\n";
+		for (int tag = 1; tag <= date.lengthOfMonth() ; tag++) {
+			System.out.printf(textFormat,tag ,date.format(fmt));
+			date = date.plusDays(1);
+		}
+		
+	}
+}
